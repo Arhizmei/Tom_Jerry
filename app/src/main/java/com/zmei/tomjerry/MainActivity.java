f3b1d7ce4916aecbf70c8f42aa8ab6ddce9a09ec
Arhizmei <arhizmei@gmail.com>
2023-05-25 16:39:31 +0300

    assignmentDone

--- app/src/main/java/com/zmei/tomjerry/MainActivity.java
+++ app/src/main/java/com/zmei/tomjerry/MainActivity.java
@@ -3,6 +3,8 @@ package com.zmei.tomjerry;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+public void eraseTom(View view){
+    ImageView imageTom = findViewById(R.id.imageTom);
+    ImageView imageJerry = findViewById(R.id.imageJerry);
+    float jerry = imageJerry.getAlpha();
+            if(jerry == 0) {
+            imageTom.animate().alpha(0).setDuration(3000);
+            imageJerry.animate().alpha(1).setDuration(3000);
+        }  else if (jerry == 1 ) {
+            imageJerry.animate().alpha(0).setDuration(3000);
+            imageTom.animate().alpha(1).setDuration(3000);
+        }
     }
 }
