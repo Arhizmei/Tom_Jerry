fecd46f5b60f7aaecd33fa0a6ef03d3056406759
arhizmei <arhizmei@gmail.com>
2023-05-26 15:36:46 +0300

    finish

--- app/src/main/java/com/zmei/tomjerry/MainActivity.java
+++ app/src/main/java/com/zmei/tomjerry/MainActivity.java
@@ -18,24 +18,24 @@ public void eraseTom(View view){
     ImageView imageTom = findViewById(R.id.imageTom);
     ImageView imageJerry = findViewById(R.id.imageJerry);
     imageTom.animate().rotation(3600).setDuration(3000);
-    imageJerry.animate().rotation(3600).setDuration(3000);
+    imageJerry.animate().rotation(360).setDuration(3000);
     float jerry = imageJerry.getAlpha();
             if(jerry == 0) {
             imageTom.animate().alpha(0).setDuration(3000);
-            imageTom.animate().rotation(-3600).setDuration(3000);
+            imageTom.animate().rotation(-360).setDuration(3000);
             imageTom.animate().scaleX(0).scaleY(0).setDuration(3000);
 
             imageJerry.animate().alpha(1).setDuration(3000);
-            imageJerry.animate().rotation(3600).setDuration(3000);
+            imageJerry.animate().rotation(360).setDuration(3000);
             imageJerry.animate().scaleX(1).scaleY(1).setDuration(3000);
 
         }  else if (jerry == 1 ) {
             imageJerry.animate().alpha(0).setDuration(3000);
-            imageJerry.animate().rotation(-3600).setDuration(3000);
+            imageJerry.animate().rotation(-360).setDuration(3000);
             imageJerry.animate().scaleX(0).scaleY(0).setDuration(3000);
 
             imageTom.animate().alpha(1).setDuration(3000);
-            imageTom.animate().rotation(3600).setDuration(3000);
+            imageTom.animate().rotation(360).setDuration(3000);
             imageTom.animate().scaleX(1).scaleY(1).setDuration(3000);
 
         }
