988a44e4be67b82dba95abd311aa916af61dec65
arhizmei <arhizmei@gmail.com>
2023-05-26 14:47:54 +0300

    TomAndJerryApperDisapear

--- app/src/main/java/com/zmei/tomjerry/MainActivity.java
+++ app/src/main/java/com/zmei/tomjerry/MainActivity.java
@@ -12,21 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageTom = findViewById(R.id.imageTom);
 
-        imageTom.animate().translationYBy(100).setDuration(3000);
-
-    }
+            }
 public void eraseTom(View view){
     ImageView imageTom = findViewById(R.id.imageTom);
     ImageView imageJerry = findViewById(R.id.imageJerry);
+    imageTom.animate().rotation(3600).setDuration(3000);
+    imageJerry.animate().rotation(3600).setDuration(3000);
     float jerry = imageJerry.getAlpha();
             if(jerry == 0) {
             imageTom.animate().alpha(0).setDuration(3000);
+            imageTom.animate().rotation(-3600).setDuration(3000);
+            imageTom.animate().scaleX(0).scaleY(0).setDuration(3000);
+
             imageJerry.animate().alpha(1).setDuration(3000);
+            imageJerry.animate().rotation(3600).setDuration(3000);
+            imageJerry.animate().scaleX(1).scaleY(1).setDuration(3000);
+
         }  else if (jerry == 1 ) {
             imageJerry.animate().alpha(0).setDuration(3000);
+            imageJerry.animate().rotation(-3600).setDuration(3000);
+            imageJerry.animate().scaleX(0).scaleY(0).setDuration(3000);
+
             imageTom.animate().alpha(1).setDuration(3000);
+            imageTom.animate().rotation(3600).setDuration(3000);
+            imageTom.animate().scaleX(1).scaleY(1).setDuration(3000);
+
         }
     }
 }
